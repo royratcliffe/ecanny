287d295815645a170658b6f0d12e6d000cc2e068
Roy Ratcliffe <royratcliffe@me.com>
2022-06-15 09:12:15 +0100

    test: dimensions_test

--- test/test_matrices.erl
+++ test/test_matrices.erl
@@ -7,3 +7,9 @@ transpose_test() ->
     ?assertEqual([[1], [2], [3]], matrices:transpose([[1, 2, 3]])),
     ?assertEqual([[1, 2, 3]], matrices:transpose([[1], [2], [3]])),
     ?assertError(badarg, matrices:transpose([[]])).
+
+dimensions_test() ->
+    ?assertEqual({0, 0}, matrices:dimensions([])),
+    ?assertEqual({1, 0}, matrices:dimensions([[]])),
+    ?assertEqual({3, 1}, matrices:dimensions([[a], [b], [c]])),
+    ?assertEqual({1, 3}, matrices:dimensions([[a, b, c]])).
