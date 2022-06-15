5b4ec2d66fa8451dcf88a61452c134c39b1ee751
Roy Ratcliffe <royratcliffe@me.com>
2022-06-15 07:48:21 +0100

    feat: transpose_test

---
+++ test/test_matrices.erl
@@ -0,0 +1,8 @@
+-module(test_matrices).
+
+-include_lib("eunit/include/eunit.hrl").
+
+transpose_test() ->
+    ?assertEqual([[1], [2], [3]], matrices:transpose([[1, 2, 3]])),
+    ?assertEqual([[1, 2, 3]], matrices:transpose([[1], [2], [3]])),
+    ?assertError(badarg, matrices:transpose([[]])).
