24dd63dafcade5960c1b3414f810fa7df48199a3
Roy Ratcliffe <royratcliffe@me.com>
2022-06-15 16:47:42 +0100

    feat: matrices (#2)
    Module `matrices` with `transpose/1` and `dimensions/1` functions.

---
+++ src/matrices.erl
@@ -0,0 +1,52 @@
+-module(matrices).
+
+-export([transpose/1, dimensions/1]).
+
+%% @type rows(). The type does <em>not</em> include the empty matrix, identical
+%% to an empty list; a matrix must possess at least one row and one column.
+%% @type dimensions(). Number of rows and number of columns, a non-negative
+%% integer tuple.
+
+-type rows() :: [[any()]].
+-type dimensions() ::
+    {NumberOfRows :: non_neg_integer(), NumberOfColumns :: non_neg_integer()}.
+
+%% @doc Transposes matrix.
+%%
+%% Recursion ends when all the rows `H' of matrix `T' match the empty list `[]'
+%% and this cannot appear as a guard condition because standard library {@link
+%% lists:all/2} cannot execute within a guard.
+%%
+%% Two sub-functions (prime and prime-prime where underscore stands for prime
+%% within the code) help to correctly match against `[]' and terminating tails,
+%% e.g. `[[], [], []]', which should fail with error if presented initially to
+%% {@link transpose/1} but successfully terminate for mapped row-vector tails.
+
+-spec transpose(Rows :: [] | rows()) -> [] | rows().
+transpose([]) -> [];
+transpose(Rows) -> transpose_(Rows).
+
+transpose_(Rows) -> [lists:map(fun hd/1, Rows) | transpose__(lists:map(fun tl/1, Rows))].
+
+transpose__(Rows) ->
+    case lists:all(fun(Row) -> Row == [] end, Rows) of
+        true -> [];
+        false -> transpose_(Rows)
+    end.
+
+%% @doc Dimensions of matrix.
+%%
+%% The resulting tuple has row-major ordering in order to align with typical
+%% row-major indices. Row comes first, column comes second.
+%%
+%% The function succeeds incorrectly for nil row vectors, e.g. `[[]]' and `[[],
+%% []]' and so on.
+
+-spec dimensions(Rows :: [] | rows()) -> dimensions().
+dimensions([]) -> {0, 0};
+dimensions([Row | Rows]) -> dimensions(Rows, 1, length(Row)).
+
+dimensions([], NumberOfRows, NumberOfColumns) -> {NumberOfRows, NumberOfColumns};
+dimensions([Row | Rows], NumberOfRows, NumberOfColumns)
+    when length(Row) == NumberOfColumns ->
+    dimensions(Rows, NumberOfRows + 1, NumberOfColumns).
---
+++ test/test_matrices.erl
@@ -0,0 +1,15 @@
+-module(test_matrices).
+
+-include_lib("eunit/include/eunit.hrl").
+
+transpose_test() ->
+    ?assertEqual([], matrices:transpose([])),
+    ?assertEqual([[1], [2], [3]], matrices:transpose([[1, 2, 3]])),
+    ?assertEqual([[1, 2, 3]], matrices:transpose([[1], [2], [3]])),
+    ?assertError(badarg, matrices:transpose([[]])).
+
+dimensions_test() ->
+    ?assertEqual({0, 0}, matrices:dimensions([])),
+    ?assertEqual({2, 0}, matrices:dimensions([[], []])),
+    ?assertEqual({3, 1}, matrices:dimensions([[a], [b], [c]])),
+    ?assertEqual({1, 3}, matrices:dimensions([[a, b, c]])).
